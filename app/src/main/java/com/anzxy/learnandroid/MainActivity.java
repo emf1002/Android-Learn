f189b65f82f98e0d60afe08ac8e4ecc48c00a15d
emf1002 <376342345@qq.com>
2016-04-14 23:14:49 +0800

    修改事件拦截界面偏移量

--- app/src/main/java/com/anzxy/learnandroid/MainActivity.java
+++ app/src/main/java/com/anzxy/learnandroid/MainActivity.java
@@ -1,13 +1,10 @@
 package com.anzxy.learnandroid;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -16,8 +13,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.anzxy.learnandroid.fragmentlifecycle.FragmentA;
-import com.anzxy.learnandroid.fragmentlifecycle.FragmentB;
 import com.anzxy.learnandroid.fragmentlifecycle.LifecycleFragment;
 import com.anzxy.learnandroid.touchevent.TouchEventFragment;
 
